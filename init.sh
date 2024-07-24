3d61530d8c90a83ba216c0b63a0eb4044d90490d
parhamyasiny <amirtaherkhani@outlook.com>
2024-07-25 00:00:32 +0330

    first commit

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir -p ./src/data/dms/mail-data/
+mkdir -p ./src/data/dms/mail-state/
+mkdir -p ./src/data/dms/mail-logs/
+mkdir -p ./src/data/dms/config/
+mkdir -p ./src/data/letsencrypt
+
+chmod 755 ./src/
+chmod 755 ./src/*
